d7828714ab804d737f0e73f6958ad114b08f08a0
marietolteca00 <Marietolteca@gmail.com>
2025-08-13 12:32:35 -0700

    for loop practice

--- conditionals-ifs-fors.R
+++ conditionals-ifs-fors.R
@@ -67,3 +67,85 @@ switch(species,
        "mouse" = print("Squeak")
        )
 # Next week we will see dplyr::case_when()!
+
+# Writing for-loops
+
+dog_names <- c("Teddy", "Kora", "Banjo", "Waffle") # Vector
+
+print("My dog's name is Teddy")
+
+# Create example for one data element to put into for loop
+# The long version with copying and pasting
+print(paste("My dog's name is", dog_names[1]))
+print(paste("My dog's name is", dog_names[2]))
+print(paste("My dog's name is", dog_names[3]))
+print(paste("My dog's name is", dog_names[4]))
+
+#storing number with variable
+pupster <- dog_names[1]
+
+print(paste("My dog's name is", pupster))
+
+# Convert to a for loop
+for (pupster in dog_names) {
+  print(paste("My dog's name is", pupster))
+}
+
+# Example 3 for loop 
+mass <- seq(from = 0, to = 3, by = 0.5)
+
+i <- mass[1]
+i
+
+new_val <- i+2 # Adding 2 to each value
+print(new_val) # printing the new value
+
+# Write into a for loop
+for (i in mass){
+  new_val <- i+2 # Adding 2 to each value
+  print(new_val) # printing the new value
+}
+
+# Practice indexing with mass example
+i <- 1
+new_val <- mass[i] + 2
+print(new_val)
+
+# Write this example into a for loop
+
+
+for (i in 1:length(mass)) { # Defining iterator using the length of vector
+  new_val <- mass[i] + 2
+  print(new_val)
+}
+
+# OR with sequence along- SAME THING as above
+
+for (i in seq_along(mass)) { # Defining iterator using seq_along()
+  new_val <- mass[i] + 2
+  print(new_val)
+}
+
+# Example 4- Iteratoring by position (indexing)
+tree_height <- c(1, 2, 6, 10)
+# Example for the first case
+# Adding element 1 to element 2 = 3
+tree_height[1] + tree_height[2]
+
+#Convert into generalizable expression- TESTING!!
+i <- 1 # Can replace with values from tree height to make sure sequence is correct
+new_val <- tree_height[i] + tree_height[ i + 1]
+print(new_val) # If it works- use in for loop. Copy and past without i <- 1
+
+
+# Convert into a for loop
+# Test out creating the sequence
+
+seq_along(tree_height)
+
+# for loop
+
+for (i in seq_along(tree_height)) {
+  new_val <- tree_height[i] + tree_height[ i + 1]
+  print(new_val) # Last value is NA because we don't have 5 values, only 4!
+}
